c114545d2809472e9c715cfeb1e15b90303c400e
Matt Connelly <gunnarstahl007@gmail.com>
2026-01-06 20:16:16 -0500

    Real policy-as-code enforced S3 deployment

--- policies/security/s3.rego
+++ policies/security/s3.rego
@@ -1,7 +1,13 @@
-package aws.s3
+package security.s3
 
 deny[msg] {
-  input.resource_type == "aws_s3_bucket"
+  input.type == "aws_s3_bucket"
   not input.encrypted
-  msg := "S3 bucket must be encrypted"
+  msg := "S3 buckets must be encrypted"
+}
+
+deny[msg] {
+  input.type == "aws_s3_bucket"
+  input.public == true
+  msg := "S3 buckets must not be public"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "good_bucket" {
-  bucket = "circleci-good-bucket-${random_string.suffix.result}"
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "circleci-policy-${random_string.suffix.result}"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
-  bucket = aws_s3_bucket.good_bucket.id
+  bucket = aws_s3_bucket.bucket.id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -16,7 +33,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
   }
 }
 
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
